51461111c23ea05570f41765f944553772bf2604
mr. whalen <michael.whalen@sitechhs.com>
2024-02-02 09:52:02 -0500

    dfgh

---
+++ project/src/components/TheWelcome.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1 v-if="test === true">Good</h1>
+    <h1 v-else>Bad</h1>
+    <h2 v-for="animal in animals" :key="animal">{{ animal }}</h2>
+  </div>
+</template>
+
+<script setup>
+const animals = ["zebra", "camel", "ape"];
+const test = false;
+</script>
+
+<style scoped></style>
