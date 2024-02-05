82558856637536c67f49ecd5eaf3c913dc1ef9ba
mr. whalen <michael.whalen@sitechhs.com>
2024-02-05 09:16:16 -0500

    ready

---
+++ project/src/components/DestCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>{{ Destination.name }}</h1>
+    <img :src="Destination.img" alt="" />
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  Destination: Object,
+});
+</script>
+
+<style scoped>
+img {
+  width: 250px;
+  height: 300px;
+  object-fit: cover;
+}
+</style>
